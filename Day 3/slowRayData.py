f6aa34b50ab1db394ce88c224f9a669e904b81cd
CodesWithTaha <i230532@isb.nu.edu.pk>
2024-07-02 02:02:45 +0500

    Day 3 added and Members of Ray added

---
+++ Day 3/slowRayData.py
@@ -0,0 +1,31 @@
+import ray
+import time
+import numpy as np
+from typing import Dict
+
+# Initialize Ray
+ray.init()
+startTime = time.time()
+# Load the Iris dataset
+from sklearn.datasets import load_iris
+data = load_iris(as_frame=True)
+df = data.frame
+
+# Create a Ray Dataset from the DataFrame
+ds = ray.data.from_pandas(df)
+
+# Define the transformation function
+def transform_batch(batch: Dict[str, np.ndarray]) -> Dict[str, np.ndarray]:
+    vec_a = batch["petal length (cm)"]
+    vec_b = batch["petal width (cm)"]
+    batch["petal area (cm^2)"] = vec_a * vec_b
+    return batch
+
+# Apply the transformation
+transformed_ds = ds.map_batches(transform_batch)
+
+# Materialize and print the transformed dataset
+print(transformed_ds.materialize())
+print("Time Taken :", time.time() - startTime)
+# Shutdown Ray
+ray.shutdown()
